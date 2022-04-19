35db34a999959815db5873493b23d84299335827
Samarth Aggarwal <samarthaggarwal2510@gmail.com>
2022-04-18 20:04:06 -0500

    updated parameter range for expt.sh

--- src/galil-mayer/expt.sh
+++ src/galil-mayer/expt.sh
@@ -12,22 +12,31 @@ if [ -f $CSVFILE ]; then
 	echo "$CSVFILE already exists"
 	exit 1
 fi
+mkdir -p $LOGDIR
 
 # range
-range_n=($(seq 10 50 40))
-range_p=($(seq 0 0.9 1))
-num_trials=2
+range_n=($(seq 5 5 45) $(seq 50 50 450) $(seq 500 100 900) $(seq 1000 1000 10000))
+range_p=($(seq 0 0.1 0.99))
+num_trials=20
 logidx=0
 logfile=${LOGDIR}/${logidx}.log
+
+echo "range_n=${range_n[@]}"
+echo "range_p=${range_p[@]}"
+echo "num_trials=${num_trials}"
 
 # headers
 echo "numNodes,maxFaults,failProb,realFaults,honestMsgCount,failedMsgCount,totalMsgCount,multiplier" > $CSVFILE
 
 for n in ${range_n[@]}; do
-	for f in $(seq 0 1 $n); do
+	#range_f=$(seq 0 1 $n)
+	range_f=(0 1 2 `let "y=n/4"; echo $y` `let "y=n/2"; echo $y` `let "y=3*n/4"; echo $y` `let "y=n-1"; echo $y`)
+	range_f=($(echo ${range_f[@]} | tr ' ' '\n' | sort | uniq | tr '\n' ' '))
+	for f in ${range_f[@]}; do
 		for p in ${range_p[@]}; do
 			for try in $(seq ${num_trials}); do
 				#echo "go run main.go $n $f $p"
+				echo "n=${n},f=${f},p=${p},try=${try}"
 				echo "#### n=${n},f=${f},p=${p},try=${try} ####" > $logfile
 				go run main.go $n $f $p >> $logfile 2>>$CSVFILE
 
